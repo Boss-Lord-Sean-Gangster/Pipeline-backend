367fa35085073d8cecbf09ffd19a64194b8c3465
Boss-Lord-Sean-Gangster <153370049+Boss-Lord-Sean-Gangster@users.noreply.github.com>
2024-12-04 20:56:51 +0530

    changes

--- __init__/auth.py
+++ __init__/auth.py
@@ -48,13 +48,13 @@ def authenticate_user(db: Session, email: str, password: str):
         return user
     return None
 
-# auth = {
-#     "hash_password": hash_password,
-#     "verify_password": verify_password,
-#     "create_access_token": create_access_token,
-#     "create_user": create_user,
-#     "authenticate_user": authenticate_user,
-#     "SECRET_KEY": SECRET_KEY,
-#     "ALGORITHM": ALGORITHM,
-#     "ACCESS_TOKEN_EXPIRE_MINUTES": ACCESS_TOKEN_EXPIRE_MINUTES
-# }
+auth = {
+    "hash_password": hash_password,
+    "verify_password": verify_password,
+    "create_access_token": create_access_token,
+    "create_user": create_user,
+    "authenticate_user": authenticate_user,
+    "SECRET_KEY": SECRET_KEY,
+    "ALGORITHM": ALGORITHM,
+    "ACCESS_TOKEN_EXPIRE_MINUTES": ACCESS_TOKEN_EXPIRE_MINUTES
+}
